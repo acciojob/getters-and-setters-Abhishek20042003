cac04a86f9af72eb5d7019d17e51e54290074b59
Abhishek20042003 <141939848+Abhishek20042003@users.noreply.github.com>
2025-10-29 21:44:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
 //complete this code
 class Person {
-	constructor(name, age){
-		this._name = name;
-		this._age = age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-	get name(){
-		return this._name;
-	}
+  get name() {
+    return this._name;
+  }
 
-	set name(username){
-		if(username.length > 0){
-				this._name = username;
-		}else{
-			console.log("Username cannot be empty")
-		}
-	}
+  get age() {
+    return this._age;
+  }
+
+  set age(newAge) {
+    if (newAge > 0) {
+      this._age = newAge;
+    } else {
+      console.log("Please enter a valid age");
+    }
+  }
 }
-
-get age(){
-	return this._age
-}
-
-set age(userage){
-	if(userage > 0){
-		this._age = userage;
-	}
-}
-
-
 
 class Student extends Person {
-	study(){
-		console.log(`${this._name} is studying`)
-		
-	}
-	
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this._name} is teaching`)
-	}
-	
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
+
+
 
 // Do not change the code below this line
 window.Person = Person;
